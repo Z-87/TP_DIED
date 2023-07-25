616eba75bb7b753896c96364bf41cc07b91ce245
FerFranzen <fer16franzen@gmail.com>
2023-07-25 20:38:11 -0300

    Clases agregadas

---
+++ src/modelo/ESTADO_SUCURSAL.java
@@ -0,0 +1,5 @@
+package modelo;
+
+public class ESTADO_SUCURSAL {
+    
+}
--- src/modelo/Ruta.java
+++ src/modelo/Ruta.java
@@ -1,5 +1,11 @@
 package modelo;
 
 public class Ruta {
-  
+    String id_ruta;
+    Centro_Logistico origen;
+    Centro_Logistico destino;
+    int capacidad;
+    Date duracion;
+    ESTADO_RUTA estado;
+    
 }
