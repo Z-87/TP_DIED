072463d13d3247cd4896855f4b15a1eb3ce2cd07
Zama <juanzamateo@gmail.com>
2023-07-28 17:26:39 -0300

    productos

--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -5,4 +5,28 @@ public class Producto implements Gestor {
   private String descripcion;
   private Double precioUnitario;
   private Double precioKg;
+
+  public Producto() {
+  }
+
+  public Producto(String nombre, String descripcion, Double precioUnitario, Double precioKg) {
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.precioUnitario = precioUnitario;
+    this.precioKg = precioKg;
+  }
+  
+  public String getNombre() {
+    return nombre;
+  }
+  public String getDescripcion() {
+    return descripcion;
+  }
+  public Double getPrecioUnitario() {
+    return precioUnitario;
+  }
+  public Double getPrecioKg() {
+    return precioKg;
+  }
+
 }
